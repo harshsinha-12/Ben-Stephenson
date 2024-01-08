14e0438d0ecdb1647e3150d0b8d0560fb5320833
Harsh Sinha <sinha.harshsep@gmail.com>
2024-01-08 19:22:51 +0530

    Ben Stephenson - The Python Workbook

---
+++ Ch-1/Ex-3.py
@@ -0,0 +1,4 @@
+l = float(input("Enter the length of the room in meters: "))
+b = float(input("Enter the breadth of the room in meters: "))
+area = l * b
+print(f"The area of the room is {area} square meters.")
