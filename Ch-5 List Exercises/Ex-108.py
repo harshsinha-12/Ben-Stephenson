e4d1e6122b875e91886f05d141dd9cc9eb47e460
Harsh Sinha <sinha.harshsep@gmail.com>
2024-02-06 13:32:33 +0530

    Ben Stephenson - The Python Workbook

--- Ch-5 List Exercises/Ex-108.py
+++ Ch-5 List Exercises/Ex-108.py
@@ -1,9 +1,9 @@
-def group_sort()-> list:
+def group_sort(lst_neg: list, lst_pov:list, lst_zero:list)-> list:
     lst_neg = []
     lst_pov = []
     lst_zero = []
     while True:
-        num = input("Enter a number (blank to stop): ")
+        num : str = input("Enter a number (blank to stop): ")
         if num == '':
             break
         elif int(num) < 0:
@@ -16,6 +16,6 @@ def group_sort()-> list:
     return lst_neg + lst_zero + lst_pov
 
 def main():
-    print("List:", group_sort())
+    print("List:", group_sort(lst_neg=[], lst_pov=[], lst_zero=[]))
 
 main()
